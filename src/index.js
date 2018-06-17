9c18b93d87252282073dcb7ae3641366f51824d5
Steffany <kielen@gmail.com>
2018-06-17 14:55:03 -0700

    completed intro protoype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+var luckyNumber = () => {
+    return Math.floor(Math.random() * 1001);
+};
+const user = {
+    'name': 'Jenny',
+    'luckyNumber': luckyNumber(),
+};
+function greeting(user){
+    let parentDiv = <div className="container">
+        <h1>Hello {user.name}!</h1>
+        <h2 className="text-muted">Your lucky number is {user.luckyNumber}!</h2>
+    </div>;
+    
+    return parentDiv;
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+
+
+
