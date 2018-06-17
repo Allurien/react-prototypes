d36e23a510d09156b7b344ca8f3ec9c8ea925ed6
Steffany <kielen@gmail.com>
2018-06-17 16:09:51 -0700

    completed functional component prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,34 @@
+import React from 'react';
+import Table from './table';
 
+const App = () => {
+    const students = [
+        {
+            'name': 'Jenny',
+            'course': 'Gymnastics',
+            'grade': '89'
+        },
+        {
+            'name': 'Johanna',
+            'course': 'Nursing',
+            'grade': '98'
+        },
+        {
+            'name': 'Jessie',
+            'course': 'Spanish',
+            'grade': '92'
+        }
+    ];
+    return (
+        <div className ="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students} />
+        </div>
+    )
+};
+
+
+
+
+export default App;
 
